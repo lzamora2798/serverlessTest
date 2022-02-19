3ebed4180ad1411cf025e2ed76ca645f3ad956cd
Luis Zamora <lzamora@espol.edu.ec>
2022-02-19 10:12:22 -0500

    changes for s3

--- src/handlerThree.py
+++ src/handlerThree.py
@@ -6,17 +6,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def sendEmail(email,text):
+   return "Sending email to {0} with the name of the log '{1}'".format(email,text) 
+
 def upload(event,context):
-    s3 = boto3.client('s3',
-        aws_access_key_id='server',
-    aws_secret_access_key='server'
-    )
+    s3 = boto3.client('s3', endpoint_url='http://localhost:4002')
     with open("resources/ex1.csv","rb") as f:
         s3.upload_fileobj(f, "local-bucket", "OBJECT_NAME")
-    # print(s3.get_available_subresources())
-    body = {"message": "hi"}
+    body = {"message": sendEmail("luis.zv27@gmail.com","app2-log")}
     return {"statusCode": 200, "body": json.dumps(body)}
 
 def email(event, context):
-    print(event,"luis")
-    print(context)
+    object_info = event["Records"][0]["s3"]["object"]
+    object_name = object_info["key"]
+    print(object_name)
